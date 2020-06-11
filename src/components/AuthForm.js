a69626abfafe9ec079463e270e596300a0386aea
toure <maka.toure@crea-inseec.com>
2020-06-11 15:34:22 +0200

    add authForm

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,44 +1,52 @@
-import React, {useState} from 'react'; 
+import React, {useState, useEffect} from 'react'; 
 import axios from 'axios'; 
 
 export default function AuthForm ({role}){
-    const [authInfo, setAuthInfo] = useState({
-        username: "", 
+    const [jwt, setJwt] = useState({
+        identifier: "", 
         password:""
     })
+    const [user, setUser] = useState([]);
 
     const handleChange = e => {
-       setAuthInfo({
-           ...authInfo,
+        setJwt({
+           ...jwt,
            [e.target.name]: e.target.value
        })
     }
    
     const handleSubmit = e =>{
         
-        e.prevenDefault();
-        console.log(authInfo)
-        axios.post(`https://strapi-crea.5ika.org/auth/${role}`, authInfo)
-            .then(res =>
-           localStorage.setItem("token", res.data.token)
-            )
+      
+        console.log(jwt)
+        axios.post(`https://strapi-crea.5ika.org/auth/local${role}`, jwt)
+            .then(res => {
+            setJwt(res.data.jwt); 
+            setUser(res.data.user); 
+            })
             .catch(err => console.log(err))    
             
     }
+
+    useEffect(() =>{
+        handleSubmit();
+    });
+    
     return ( 
         <div>
             <h1>please {role}</h1>
             <form onSubmit={handleSubmit}>
                 <input  
                     name="username"
-                    value={authInfo.username}
+                    type="text"
+                    value={jwt.identifier}
                     onChange={handleChange}
                     placeholder="votre nom"
                     />
                 <input
                     name="password"
                     type="password"
-                    value={authInfo.password}
+                    value={jwt.password}
                     onChange={handleChange} 
                     />
                     <button type="submit">Connection</button>
